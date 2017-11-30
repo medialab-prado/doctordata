63ca028652ec473079ccbedc9cfa0b3b865d5bfe
Rai <rai@ingmechs.com>
2017-11-30 12:33:34 +0100

    Added missing install

--- bot/install_missing.sh
+++ bot/install_missing.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install git
+sudo apt-get -y install python-dev build-essential
+sudo apt-get -y install python3-dev
+sudo apt-get -y install python-pip python3-pip
+sudo apt-get install python3-setuptools
+sudo pip install --upgrade pip
 
+sudo pip install pandas
+sudo pip3 install pandas
 sudo pip install geopy
 sudo pip install
 sudo pip install python-telegram-bot --upgrade
